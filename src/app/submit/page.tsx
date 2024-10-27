3c9ada5cc818c337133e129c905309fd824b047e
AxiomaticAdi <arudra@bu.edu>
2024-10-27 10:25:10 -0700

    add layout to submit page

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -2,6 +2,7 @@
 
 import MealSubmitFormCard from "@/components/custom/MealSubmitFormCard";
 import MealReportCard from "@/components/custom/MealReportCard";
+import Layout from "@/components/custom/Layout";
 import { useState } from "react";
 import { MealResponse } from "@/types/mealTypes";
 export default function SubmitPage() {
@@ -11,18 +12,20 @@ export default function SubmitPage() {
 	const [mealResponse, setMealResponse] = useState<MealResponse | null>(null);
 
 	return (
-		<div className="flex flex-col justify-center items-center gap-4">
-			<MealSubmitFormCard
-				mealType={mealType}
-				mealDescription={mealDescription}
-				setMealType={setMealType}
-				setMealDescription={setMealDescription}
-				setIsSubmit={setIsSubmit}
-				setMealResponse={setMealResponse}
-			/>
-			{isSubmit && mealResponse && (
-				<MealReportCard mealResponse={mealResponse} />
-			)}
-		</div>
+		<Layout>
+			<div className="flex flex-col justify-center items-center gap-4 pt-12 md:pt-24">
+				<MealSubmitFormCard
+					mealType={mealType}
+					mealDescription={mealDescription}
+					setMealType={setMealType}
+					setMealDescription={setMealDescription}
+					setIsSubmit={setIsSubmit}
+					setMealResponse={setMealResponse}
+				/>
+				{isSubmit && mealResponse && (
+					<MealReportCard mealResponse={mealResponse} />
+				)}
+			</div>
+		</Layout>
 	);
 }
